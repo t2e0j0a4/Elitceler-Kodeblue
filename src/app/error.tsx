647a3da69fa4d6bdfe7611ab5d919ebbd9a14ecf
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-03-24 16:53:29 +0530

    not-found and error pages added

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,25 @@
-'use client';
-import React from 'react'
+'use client' 
+ 
+import Link from 'next/link';
+import Image from 'next/image';
+import { useEffect } from 'react';
+ 
+export default function Error({ error, reset }: { error: Error & { digest?: string }, reset: () => void }) {
 
-const error = () => {
+  useEffect(() => {
+    console.error(error);
+  }, [error])
+ 
   return (
-    <div>error</div>
+    <main className='app__error'>
+      <div className='error__center'>
+        <Image src="/others/ServerDown.svg" priority={false} alt='Error Occured' width={500} height={380} />
+        <h2>Something Went Wrong!</h2>
+        <div className='error__options'>
+          <button type="button" onClick={() => reset()}>Try Again!</button>
+          <Link href="/dashboard" title='Dashboard'>Go To Dashboard</Link>
+        </div>
+      </div>
+    </main>
   )
 }
-
-export default error
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <main className='app__notFound'>
+      <div className='notFound__center'>
+        <h1>404</h1>
+        <Image src={"/others/NotFound.svg"} alt='Not Found 404' width={500} height={280} priority={false} />
+        <p>You&#39;re navigated to a Broken URL.</p>
+        <Link href="/dashboard" title='Dashboard'>Back Dashboard</Link>
+      </div>
+    </main>
   )
 }
 
-export default NotFound
+export default NotFound;
