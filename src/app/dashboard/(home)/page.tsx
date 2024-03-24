1a077af0c7b6ab0237d52d40bea57b01c6852432
Nagateja Valiveti <teja91.valiveti@gmail.com>
2024-03-24 17:49:08 +0530

    dashboard UI

--- src/app/dashboard/(home)/page.tsx
+++ src/app/dashboard/(home)/page.tsx
@@ -1,10 +1,14 @@
 import styles from "./page.module.css";
 
+// Components
+import Topbar from "@/components/dashboard/Topbar/Topbar";
+
 const DashboardHome = () => {
   return (
-    <main className={styles.app__home}>
-      Hospital Dashboard
-    </main>
+    <div className={styles.app__home}>
+      <Topbar/>
+      <section className={styles.home__center}></section>
+    </div>
   )
 }
 
